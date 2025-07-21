ad36281ea99863d2836837810e6172fc1f75f469
MartaAlerany <marta.alerany@bsc.es>
2025-07-21 14:36:31 +0200

    Removed redundant files

--- build/lib/pyhanami/__init__.py
+++
@@ -1,8 +0,0 @@
-from .diags.Simulations import SimulationData
-from .diags.Observations import ObservationData
-from .diags.Diagnostics import DataDiagnostics
-from .diags.Replicability import ReplicabilityTest
-
-__all__ = [
-    "SimulationData", "ObservationData", "DataDiagnostics", "ReplicabilityTest"
-]
