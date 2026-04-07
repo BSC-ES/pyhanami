e030c45970359b3a3c92d868d7f32080cdca1f09
MartaAlerany <marta.alerany@bsc.es>
2026-04-07 17:51:40 +0200

    Updated release version in project and environments files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'Pyhanami'
 copyright = '2025, Marta Alerany Solé, Kai Keller'
 author = 'Marta Alerany Solé, Kai Keller'
-release = '0.1.0'
+release = '0.2.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
